0f8be8f80c32a6febc59cbdebcf101539b70f33b
Eko Kurniawan <57644404+ekokurnia@users.noreply.github.com>
2025-04-30 21:32:42 +0700

    feat: hide premium menu from sidebar on user dashboard, includes : captain, sla report, custom role and sla settings

--- app/javascript/dashboard/components-next/sidebar/Sidebar.vue
+++ app/javascript/dashboard/components-next/sidebar/Sidebar.vue
@@ -198,28 +198,28 @@ const menuItems = computed(() => {
         },
       ],
     },
-    {
-      name: 'Captain',
-      icon: 'i-woot-captain',
-      label: t('SIDEBAR.CAPTAIN'),
-      children: [
-        {
-          name: 'Assistants',
-          label: t('SIDEBAR.CAPTAIN_ASSISTANTS'),
-          to: accountScopedRoute('captain_assistants_index'),
-        },
-        {
-          name: 'Documents',
-          label: t('SIDEBAR.CAPTAIN_DOCUMENTS'),
-          to: accountScopedRoute('captain_documents_index'),
-        },
-        {
-          name: 'Responses',
-          label: t('SIDEBAR.CAPTAIN_RESPONSES'),
-          to: accountScopedRoute('captain_responses_index'),
-        },
-      ],
-    },
+    // {
+    //   name: 'Captain',
+    //   icon: 'i-woot-captain',
+    //   label: t('SIDEBAR.CAPTAIN'),
+    //   children: [
+    //     {
+    //       name: 'Assistants',
+    //       label: t('SIDEBAR.CAPTAIN_ASSISTANTS'),
+    //       to: accountScopedRoute('captain_assistants_index'),
+    //     },
+    //     {
+    //       name: 'Documents',
+    //       label: t('SIDEBAR.CAPTAIN_DOCUMENTS'),
+    //       to: accountScopedRoute('captain_documents_index'),
+    //     },
+    //     {
+    //       name: 'Responses',
+    //       label: t('SIDEBAR.CAPTAIN_RESPONSES'),
+    //       to: accountScopedRoute('captain_responses_index'),
+    //     },
+    //   ],
+    // },
     {
       name: 'Contacts',
       label: t('SIDEBAR.CONTACTS'),
@@ -298,16 +298,16 @@ const menuItems = computed(() => {
           label: t('SIDEBAR.CSAT'),
           to: accountScopedRoute('csat_reports'),
         },
-        {
-          name: 'Reports SLA',
-          label: t('SIDEBAR.REPORTS_SLA'),
-          to: accountScopedRoute('sla_reports'),
-        },
-        {
-          name: 'Reports Bot',
-          label: t('SIDEBAR.REPORTS_BOT'),
-          to: accountScopedRoute('bot_reports'),
-        },
+        // {
+        //   name: 'Reports SLA',
+        //   label: t('SIDEBAR.REPORTS_SLA'),
+        //   to: accountScopedRoute('sla_reports'),
+        // },
+        // {
+        //   name: 'Reports Bot',
+        //   label: t('SIDEBAR.REPORTS_BOT'),
+        //   to: accountScopedRoute('bot_reports'),
+        // },
       ],
     },
     {
@@ -445,24 +445,24 @@ const menuItems = computed(() => {
           icon: 'i-lucide-blocks',
           to: accountScopedRoute('settings_applications'),
         },
-        {
-          name: 'Settings Audit Logs',
-          label: t('SIDEBAR.AUDIT_LOGS'),
-          icon: 'i-lucide-briefcase',
-          to: accountScopedRoute('auditlogs_list'),
-        },
-        {
-          name: 'Settings Custom Roles',
-          label: t('SIDEBAR.CUSTOM_ROLES'),
-          icon: 'i-lucide-shield-plus',
-          to: accountScopedRoute('custom_roles_list'),
-        },
-        {
-          name: 'Settings Sla',
-          label: t('SIDEBAR.SLA'),
-          icon: 'i-lucide-clock-alert',
-          to: accountScopedRoute('sla_list'),
-        },
+        // {
+        //   name: 'Settings Audit Logs',
+        //   label: t('SIDEBAR.AUDIT_LOGS'),
+        //   icon: 'i-lucide-briefcase',
+        //   to: accountScopedRoute('auditlogs_list'),
+        // },
+        // {
+        //   name: 'Settings Custom Roles',
+        //   label: t('SIDEBAR.CUSTOM_ROLES'),
+        //   icon: 'i-lucide-shield-plus',
+        //   to: accountScopedRoute('custom_roles_list'),
+        // },
+        // {
+        //   name: 'Settings Sla',
+        //   label: t('SIDEBAR.SLA'),
+        //   icon: 'i-lucide-clock-alert',
+        //   to: accountScopedRoute('sla_list'),
+        // },
         {
           name: 'Settings Billing',
           label: t('SIDEBAR.BILLING'),
